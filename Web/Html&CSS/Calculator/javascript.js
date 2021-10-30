794388b5b267bc59067d65244d3d7cd85062ab8d
Luiz Felipe <73553939+FelipeDevMelo@users.noreply.github.com>
2021-10-30 10:01:16 -0300

    finished calculator

--- Web/Html&CSS/Calculator/javascript.js
+++ Web/Html&CSS/Calculator/javascript.js
@@ -1,56 +1,16 @@
-var num1, num2, varBtn, operation
-var text = document.getElementById("text")
-var alreadyClick = 0
-var alreadyCalc = false
 
-function ChooseAction(varBtn) {
-  if (varBtn == 10) {
-    text.innerHTML = "0"
-    alreadyClick = false
-    alreadyClick = 0
+
+function ChooseAction(num) {
+  var text = document.getElementById("text")
+  text.innerHTML+=num
   }
-  if (alreadyClick == 0) {
-    text.innerHTML = varBtn
-    num1 = varBtn
-    alreadyClick++
-  } else if (alreadyClick == 1) {
-    if (varBtn == 11) {
-      text.innerHTML += " / "
-      operation = "/"
-    } else if (varBtn == 12) {
-      text.innerHTML += " X "
-      operation = "X"
-    } else if (varBtn == 14) {
-      text.innerHTML += " + "
-      operation = "+"
-    } else if (varBtn == 15) {
-      text.innerHTML += " - "
-      operation = "-"
+   function clean (){
+    document.getElementById("text").innerHTML=""
+   }
 
+   function calculate(){
+    var text = document.getElementById("text").innerHTML
+    if(text){
+     document.getElementById("text").innerHTML = eval(text)
     }
-    alreadyClick++
-  } else if (alreadyClick == 2) {
-    text.innerHTML += varBtn
-    alreadyClick++
-  } else if (alreadyClick == 3) {
-    if (varBtn == 13) {
-      if (alreadyCalc) {
-        switch (operation) {
-          case "/": text.innerHTML = num1 / num2
-        }
-      }
     }
-
-
-
-
-  }
-}
-/*
-10: c
-11: /
-12: x
-13: =
-14: +
-15 : -
-    */

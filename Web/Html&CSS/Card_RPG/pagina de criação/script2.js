38a1d2e2e5649690108f3281511357bc8eec9fd4
Luiz Felipe <luizfelipemelo001@gmail.com>
2021-11-30 19:23:49 -0300

    aaa

--- Web/Html&CSS/Card_RPG/pagina de criação/script2.js
+++
@@ -1,7 +0,0 @@
-function Submit(){
-var selectRaca = document.getElementById('raçaInput');
-var valueRaca = selectRaca.options[selectRaca.selectedIndex].value;
-var selectClasse = document.getElementById('classeInput');
-var valueClasse = selectClasse.options[selectClasse.selectedIndex].value;
-window.alert("raça:  "+valueRaca+"   Classe: "+valueClasse); // en
-}
